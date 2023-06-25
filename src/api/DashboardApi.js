a0aab3865e8b7ce610985a1e0b0056b97b32c7b0
D MUKUNZI <32438995+desiremukunzi@users.noreply.github.com>
2023-06-25 22:21:59 +0200

    Updated the baseURL with the https domaine

--- src/api/DashboardApi.js
+++ src/api/DashboardApi.js
@@ -2,5 +2,5 @@
 
  export default axios.create({
 
-    baseURL:"http://159.223.111.104:8055/report/daily-report-by-service",
+    baseURL:"https://apiportal.koipay.co/report/daily-report-by-service",
  })

2bbb74f17e7993777cc3774e6af090cc6cfedbba
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2023-09-27 17:14:32 +0200

    fixing status issue

--- src/components/dashboard/parkpick/pages/ParkPick.js
+++ src/components/dashboard/parkpick/pages/ParkPick.js
@@ -54,17 +54,18 @@ const ParkPick = () => {
     const categoriesList = useSelector(getAllparkCategories);
 
     const boughtItemsList = useSelector(getAllparkPickBoughtItemsList);
+   
     const nonPaginatedItemsList = useSelector(getAllNonPaginatedItems);
-    const total = boughtItemsList.reduce((accumulator, item) => {
+    const total =boughtItemsList ? boughtItemsList.reduce((accumulator, item) => {
         // Assuming that item.number is a number you want to sum
         return accumulator + item.number;
-    }, 0);
+    }, 0):[];
     // Map through the items and calculate the amount for each item
     let totalAmount = 0;
-    const items = boughtItemsList.map((item, index) => {
+    const items =boughtItemsList? boughtItemsList.map((item, index) => {
         const amount = item.item.price * item.number;
         totalAmount += amount;
-    })
+    }):[];
 
 
     const paginatedBoughtItemsList = useSelector(getAllparkPickPaginatedBoughtItemsList)
@@ -357,7 +358,7 @@ const ParkPick = () => {
                     'Accept': 'application/json'
                 },
             });
-            dispatch(fetchAsynItems())
+            dispatch(fetchAsynItems(currentPage))
             addToast(`successful created`, {
                 appearance: 'success', autoDismiss: true, // Enable auto dismissal
                 autoDismissTimeout: 5000,
@@ -474,7 +475,7 @@ const ParkPick = () => {
                         'Accept': 'application/json'
                     },
                 });
-                dispatch(fetchAsynItems())
+                dispatch(fetchAsynItems(currentPage))
                 addToast(`deleted`, {
                     appearance: 'success', autoDismiss: true, // Enable auto dismissal
                     autoDismissTimeout: 5000,
@@ -551,7 +552,7 @@ const ParkPick = () => {
                     'Accept': 'application/json'
                 },
             });
-            dispatch(fetchAsynItems())
+            dispatch(fetchAsynItems(currentPage))
             addToast(`successful updated`, {
                 appearance: 'success', autoDismiss: true,
                 autoDismissTimeout: 5000,
@@ -1219,7 +1220,7 @@ const ParkPick = () => {
                                                 format="YYYY-MM-DD" />
                                         </div>
                                         <div className='flex flex-col' >
-                                            <span className='py-2' >Filter by Items :</span>
+                                            <span className='py-2' >Filter by Item :</span>
                                             <Select
                                                 showSearch
                                                 placeholder="Select an items"
@@ -1242,19 +1243,19 @@ const ParkPick = () => {
                                                 options={[
                                                     {
                                                         value: '200',
-                                                        label: '200',
+                                                        label: 'SUCCESS',
                                                     },
                                                     {
                                                         value: '201',
-                                                        label: '201',
+                                                        label: 'CREATED',
                                                     },
                                                     {
                                                         value: '202',
-                                                        label: '202',
+                                                        label: 'PENDING',
                                                     },
                                                     {
                                                         value: '500',
-                                                        label: '500',
+                                                        label: 'FAILED',
                                                     },
                                                 ]}
                                             />
@@ -1264,15 +1265,7 @@ const ParkPick = () => {
                                         </button> */}
                                         <ExcelExport excelData={boughtItemsList}  Amount={totalAmount} />
                                     </div>
-                                    <div className='flex justify-center items-center ' >
-                                        {isLoading && <div role="status" className='flex justify-center items-center  w-full' >
-                                            <svg aria-hidden="true" class="inline w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-red-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                                            </svg>
-                                            <span class="sr-only">Loading...</span>
-                                        </div>}
-                                    </div>
+                                  
                                     <div>
 
                                         {/* <Modal setOpenModal={editCategory} onChildEvent={handleChildEditCategoryEvent} Title={categoryupdate} button={btn_name}  >
@@ -1332,9 +1325,18 @@ const ParkPick = () => {
                                             </th>
                                         </tr>
                                     </thead>
+                                    <div className='flex  justify-center items-center spinner-w' >
+                                        {isLoading && <div role="status" className='flex justify-center items-center  w-full' >
+                                            <svg aria-hidden="true" class="inline w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-red-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                            </svg>
+                                            <span class="sr-only">Loading...</span>
+                                        </div>}
+                                    </div>
                                     <tbody>
 
-                                        {boughtItemsList.length && (boughtItemsList.map((item, index) => (
+                                        {boughtItemsList.length ? (boughtItemsList.map((item, index) => (
                                             <tr className="bg-white border-b dark:hover:bg-gray-300 dark:hover:text-black" key={index} >
                                                 <td className="w-4 p-4">
                                                     {item.dailyTransactionForMobile.internalTxnId}
@@ -1346,19 +1348,25 @@ const ParkPick = () => {
                                                     {item.item.price}
                                                 </td>
                                                 <td className="px-6 py-4">
-                                                    {item.number} /{item.unit}
+                                                    {item.number} / {item.unit}
                                                 </td>
                                                 <td className='px-6 py-4 flex justify-between' >
                                                     {item.item ? item.item.price * item.number : 'N/A'}
                                                 </td>
                                                 <td className="px-6 py-4">
-                                                {item.dailyTransactionForMobile? item.dailyTransactionForMobile.status:'N/A'}
+                                               {item.dailyTransactionForMobile.statusDesc==='FAILED' && <span className='FAILED' > FAILED</span> } <span></span>
+                                               {item.dailyTransactionForMobile.statusDesc==='PENDING' && <span className='PENDING' > PENDING</span>} <span></span>
+                                               {item.dailyTransactionForMobile.statusDesc==='SUCCESS' && <span className='SUCCESS' > SUCCESS</span>} <span></span>
+                                               {item.dailyTransactionForMobile.statusDesc==='CREATED' && <span className='CREATED' > CREATED</span>} <span></span>
+                                               {item.dailyTransactionForMobile.statusDesc==='PENDING' && <span className='PENDING' > PENDING</span>} <span></span>
                                                 </td>
                                                 <td className="px-6 py-4">
                                                     {item.dailyTransactionForMobile ? moment(item.dailyTransactionForMobile.createdAt).format('MMM Do YYYY, h:mm:ss a') : 'N/A'}
                                                 </td>
                                             </tr>
-                                        )))
+                                        ))):( <div>
+                                            no items available
+                                        </div> )
                                         }
                                         {boughtItemsList.length && (<tr className="bg-white border-b dark:hover:bg-gray-300 dark:hover:text-black"  >
                                             <td className="w-4 p-4">
@@ -1371,7 +1379,7 @@ const ParkPick = () => {
                                                 Total unit : <strong className='total-unit font-semibold text-black' >{total}</strong>
                                             </td>
                                             <td className="w-4 p-4 total-unit ">
-                                                Total amount : <strong className='total-unit font-semibold text-black' >{totalAmount}</strong>RWF
+                                                Total amount : <strong className='total-unit font-semibold text-black' >{totalAmount}</strong> RWF
                                             </td>
                                             <td className="w-4 p-4">
                                             </td>
